c449e4dd5ead0710296a2219a7dc1862643361d7
Deependra Verma <deependra.verma00@gmail.com>
2024-03-24 09:32:39 +0530

    Code and version updated

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires = get_requirements("requirements_dev.txt"),
+    install_requires = get_requirements("D:\PyMongo-Atlas-Connector-Seamless-MongoDB-Integration\requirements_dev.txt"),
     include_package_data=True,
     )
