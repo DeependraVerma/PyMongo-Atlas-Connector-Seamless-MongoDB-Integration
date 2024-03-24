000f2ae79b9ea3c3a43a60831f42a61f1683ac62
Deependra Verma <deependra.verma00@gmail.com>
2024-03-24 09:20:15 +0530

    version updated

--- setup.py
+++ setup.py
@@ -1,23 +1,15 @@
 from setuptools import setup, find_packages
-from typing import List
 
 with open('README.md', 'r', encoding='utf-8') as f:
-    long_description = f.read()     
+    long_description = f.read()
 
-HYPEN_E_DOT = "-e ."
-def get_requirements(file_path:str)-> List[str]:
-    requirements = []
-    with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n","") for req in requirements]
-
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-    return requirements
+def get_requirements(file_path):
+    with open(file_path) as f:
+        return f.read().splitlines()
 
 __version__ = "0.0.4"
 REPO_NAME = "PyMongo-Atlas-Connector-Seamless-MongoDB-Integration"
-PKG_NAME= "databaseconnection"
+PKG_NAME = "databaseconnection"
 AUTHOR_USER_NAME = "DeependraVerma"
 AUTHOR_EMAIL = "deependra.verma00@gmail.com"
 
@@ -26,7 +18,7 @@ setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with database.",
+    description="A python package for connecting with a database.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
@@ -35,6 +27,9 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires = get_requirements("./requirements_dev.txt"),
+    install_requires=get_requirements("requirements.txt"),
+    extras_require={
+        'dev': get_requirements("requirements_dev.txt")
+    },
     include_package_data=True,
-    )
+)
