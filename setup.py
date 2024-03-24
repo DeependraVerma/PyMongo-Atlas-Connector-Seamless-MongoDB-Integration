44bdd7f4d3b86b0d27fa279987846c3c009feb49
Deependra Verma <deependra.verma00@gmail.com>
2024-03-24 09:23:52 +0530

    New Version  updated

--- setup.py
+++ setup.py
@@ -4,11 +4,15 @@ from typing import List
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
 
-def get_requirements(file_path:str) -> List[str]:
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path:str)-> List[str]:
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.strip() for req in requirements]
+        requirements = [req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     return requirements
 
 __version__ = "0.0.4"
@@ -22,7 +26,7 @@ setup(
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="A python package for connecting with a database.",
+    description="A python package for connecting with database.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
@@ -31,10 +35,6 @@ setup(
     },
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=get_requirements("./requirements.txt"),
-    extras_require={
-        'dev': get_requirements("./requirements_dev.txt")
-    },
+    install_requires = get_requirements("./requirements_dev.txt"),
     include_package_data=True,
-    package_data={"": ["requirements_dev.txt"]},  # Ensure requirements_dev.txt is included
-)
+    )
